0581e56383db09c4cdb6b88772b50db74f2a2cf1
Isabelle Lee <lee.isabelle.g@gmail.com>
2017-10-24 20:01:39 -0700

    lda-pipeline

---
+++ src/lda_pipe.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+import pyspark as ps
+from pyspark.ml.clustering import LDA
+import boto3
+
+def save_to_s3(filename, path):
+    s3 = boto3.client('s3')
+    s3.upload_file(Filename=filename, Bucket='outrunjulesverne',\
+                    Key=path)
+
+def lda_model_score(df,num_topics):
+    lda = LDA(k=num_topics, optimizer="em")
+    model = lda.fit(df)
+    topics = model.describeTopics().collect()
+    transformed = model.transform(df)
+    return lda, model, topics, transformed
+
+def print_topics(topics, ls):
+    for topic in topics:
+        print("- TOPIC {} -".format(topic[0]))
+        topickeys = topic[1]
+        topicvalues = topic[2]
+        for i in range(len(topickeys)):
+            print("  - word '{}': {}".format(ls[topickeys[i]],topicvalues[i]))
+
+def to_pickle(df, filename):
+    p = df.toPandas()
+    p.to_pickle(filename)
+    return p

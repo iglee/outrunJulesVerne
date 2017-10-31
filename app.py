1c2df4e6cbb555702d6908931757c6a4f6e2e6a1
Isabelle Lee <lee.isabelle.g@gmail.com>
2017-10-30 18:31:54 -0700

    webstuff 🌈

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from __future__ import division
 from flask import Flask, render_template, request, jsonify
 import numpy as np
+import pandas as pd
+import sys
+
+sys.path.insert(0, '/Users/isabellelee/Desktop/data_science/capstone/src')
+data_dir ='/Users/isabellelee/Desktop/data_science/capstone/data'
+from output import output
+
+
+from recommend import sample_df, make_tour
+from plot_rec import plot_map
 
 app = Flask(__name__)
 
@@ -12,9 +22,11 @@ def index():
 def get_topics():
     user_data = request.json
     input_topics = np.where(user_data['topics'])[0]
-
-    return str(input_topics)
-
+    tour_io, map_io, tour, map_ = output(input_topics)
+    #results = {'tour_results' : tour_io.getvalue(), 'map_results' : map_io.getvalue()}
+    #return jsonify(results)
+    #return map_io.getvalue(), 200, {'Content-Type':'image/png'}
+    return tour_io.getvalue()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
--- src/output.py
+++ src/output.py
@@ -2,16 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import StringIO
 data_dir ='/Users/isabellelee/Desktop/data_science/capstone/data'
 
 from recommend import sample_df, make_tour
 from plot_rec import plot_map
 
-world = pd.read_pickle(data_dir+"/jules_world.pkl")
-tour = make_tour(world,[1,4,17])
-map_ = plot_map(tour)
+def output(user_input):
+    world = pd.read_pickle(data_dir+"/jules_world.pkl")
+    tour = make_tour(world,user_input)
+    map_ = plot_map(tour)
+    tour = tour[['continent','country','attraction']]
+
+    tour_io = StringIO.StringIO()
+    map_io = StringIO.StringIO()
+    tour.to_html(buf=tour_io, index=False)
+    plt.savefig(map_io)
+    return tour_io, map_io, tour, map_
 
 if __name__ == "__main__":
-    world = pd.read_pickle(data_dir+"/jules_world.pkl")
-    tour = make_tour(world,[1,4,17])
-    map_ = plot_map(tour)
+    tour, map_ = output([1,4,19])
